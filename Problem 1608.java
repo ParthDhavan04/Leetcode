b11f835095003869f38c5328debfb4fefd96aff3
ParthDhavan04 <parthdhavan2004@gmail.com>
2024-05-25 16:04:59 +0530

    Special Array With X Elements Greater Than or Equal X

---
+++ Problem 1608.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        int n=nums.length;
+        
+        for(int x=1;x<=n;x++){
+            int c=0;
+            for(int j=0;j<n;j++){
+                if(nums[j] >= x)
+                c++;
+            }
+            if(c==x){
+                return x;
+            }
+           
+        }
+         
+        return -1;
+    }
+}
