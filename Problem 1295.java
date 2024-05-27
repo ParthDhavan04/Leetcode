fcc564dd8b3f222519554872913f36eed8f9b8ac
ParthDhavan04 <parthdhavan2004@gmail.com>
2024-05-27 23:44:22 +0530

    Find Numbers with Even Number of Digits

--- Problem 1295.java
+++ Problem 1295.java
@@ -16,3 +16,53 @@ class Solution {
         return temp;
     }
 }
+/* Given an array nums of integers, return how many of them contain an even number of digits. 
+
+ //Method 1 - count by conventional while loop method
+//Method 2 - use Math.log to count number of digits
+//Method 3 - converrt to string and find length of string
+
+class Solution {
+    public int findNumbers(int[] nums) {
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            int d = digits(nums[i]);
+            if(even(d))
+            c++;
+        }
+        return c;
+    }
+
+    static int digits(int n){  //M-2 instead of counting like this use 'return (int)(Math.log10(n)) + 1; '  in place of whole function
+        int d=0;
+        while(n!=0){
+            d++;
+            n/=10;
+        }
+        return d;
+    }
+
+    static boolean even(int n){
+        if(n%2==0) return true;
+        else return false;
+    }
+}
+
+
+//Method - 3
+ class Solution {
+    public int findNumbers(int[] nums) {
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            String a = Integer.toString(nums[i]);
+            int l =  a.length();
+            if(even(l))
+            c++;
+        }
+        return c;
+    }
+    static boolean even(int n){
+        if(n%2==0) return true;
+        else return false;
+    }
+} */
