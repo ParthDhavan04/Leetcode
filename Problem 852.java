d4bb2393da734d97ceb60a0ff96ab58a45043e12
ParthDhavan04 <parthdhavan2004@gmail.com>
2024-05-30 18:25:57 +0530

    Peak Index in a Mountain Array

---
+++ Problem 852.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int ans=binarySearch( arr);
+        return ans;
+    } 
+    static int binarySearch(int[] arr) {
+        int start=0;
+        int end=arr.length-1;
+
+        while(start != end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid+1] < arr[mid]) {
+                end = mid;
+            } else if(arr[mid+1]> arr[mid]) {
+                start = mid + 1;
+            } 
+        }
+     return start;
+    }
+  
+}
+        
