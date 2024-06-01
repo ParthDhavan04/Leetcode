4e8f7711a92bcc761d0fff756b6d541e54a9d059
ParthDhavan04 <parthdhavan2004@gmail.com>
2024-06-02 02:18:21 +0530

    Split Array Largest Sum

---
+++ Problem 410.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+
+        int start,end,mid=0,sum=0,max=nums[0],pieces=1;
+
+        for(int i=0;i<nums.length;i++){ 
+            sum += nums[i];            //max range = sum
+            if(nums[i]>max)
+            max = nums[i];            //min range = max number
+        }
+
+        start = max; end = sum;
+        while(start!=end){
+            mid = start + (end-start)/2; 
+            sum = 0; pieces = 1;
+            for(int i=0;i<nums.length;i++)
+            {
+                sum = sum + nums[i];
+                if(sum <= mid)
+                continue;
+                else{
+                pieces++;
+                sum=nums[i];
+                }
+            }
+            
+            if(pieces>k)
+            start = mid + 1;
+            else
+            end = mid;
+    }
+    return end;
+}}
