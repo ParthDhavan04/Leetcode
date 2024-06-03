d6ff113ea16bb2a57d6e2845ae02e77441d94871
ParthDhavan04 <parthdhavan2004@gmail.com>
2024-06-03 15:23:12 +0530

    Search in Rotated Sorted Array (duplicates)(booolean)

---
+++ Binary Search/Problem 81.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) 
+            return true;
+
+            if (nums[start] == nums[mid]) {
+                start++;
+                continue;
+            }
+
+            if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target <= nums[mid]) 
+                end = mid - 1;
+                
+                else 
+                start = mid + 1;
+            } 
+            else {
+                if (nums[mid] <= target && target <= nums[end]) 
+                start = mid + 1;
+
+                else 
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+}
