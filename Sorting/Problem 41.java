7f6600d6c37a7c0408b19d89244cc00d34d823cf
ParthDhavan04 <parthdhavan2004@gmail.com>
2024-06-08 16:11:34 +0530

    First Missing Positive

---
+++ Sorting/Problem 41.java
@@ -0,0 +1,33 @@
+class Solution {//AMAZON
+    public int firstMissingPositive(int[] nums) {
+
+        sort(nums);
+
+        int ans=nums.length+1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != i+1){
+                ans=i+1;
+                break;
+            }
+        }
+        
+        return ans;
+    }
+
+    void sort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i]-1;
+            if (arr[i]<arr.length && arr[i] >0 && arr[i] != arr[correct]) 
+                swap(arr, i , correct);
+            else 
+                i++;    
+        }
+    }
+
+    void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
