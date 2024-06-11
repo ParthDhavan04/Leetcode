c4f98a7923e0a39721c977a9bce6a0ad9ae903f5
ParthDhavan04 <parthdhavan2004@gmail.com>
2024-06-11 14:18:30 +0530

    (HARD) Valid Number

--- Strings/Problem 65.java
+++ Strings/Problem 65.java
@@ -2,8 +2,9 @@ class Solution {
     public boolean isNumber(String s) {
 
         boolean numberSeen = false;
-        boolean eSeen = false;
-        boolean decSeen = false;
+        boolean eSeen = false;//if e or E has been seen
+        boolean decSeen = false; //decimal encountered or not
+
         
         for(int i=0; i<s.length(); i++){
 
@@ -12,20 +13,21 @@ class Solution {
             if(Character.isDigit(ch)){
                 numberSeen=true;
             }
-            else if(ch=='e' || ch=='E'){
+            else if(ch=='e' || ch=='E'){//need another number now after E / e
+
                 if(eSeen || !numberSeen){
                     return false;
                 }
                 eSeen=true;
                 numberSeen=false;
             }
-            else if(ch=='.'){
+            else if(ch=='.'){//if dot has been seen, cannot see another dot & cannot have E/e followed by decimal 99e2.5
                 if(decSeen || eSeen){
                     return false;
                 }
                 decSeen=true;
             }
-            else if(ch=='-' || ch =='+'){
+            else if(ch=='-' || ch =='+'){//if 3e+7 then e followed by +/- is allowed or e should never be any other place except start
                 if(i>0 && s.charAt(i-1)!= 'e' && s.charAt(i - 1) != 'E')
                 return false;
             }
@@ -34,6 +36,7 @@ class Solution {
             }
 
         }
-        return numberSeen;
+        return numberSeen; //number has to be seen once ie true
+
     }
 }
