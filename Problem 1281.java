cb7abd2baa75064476b773505a607cf57321a522
ParthDhavan04 <parthdhavan2004@gmail.com>
2024-06-14 21:10:15 +0530

    Subtract the Product and Sum of Digits of an Integer

---
+++ Problem 1281.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subtractProductAndSum(int n) {
+
+        int pro=1,sum=0;
+
+        while(n!=0)
+        {
+        sum=sum+(n%10);
+        pro=pro*(n%10);
+        n=n/10;
+        }
+        
+        return pro-sum;
+        
+    }
+}
