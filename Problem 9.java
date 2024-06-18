e6b9b4407ddfa1803713dfc7455e713a5956ae81
Parth Dhavan <parthdhavan2004@gmail.com>
2024-06-18 23:22:48 +0530

    Palindrome Number

---
+++ Problem 9.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int n) {
+         if(n<0)
+        return false;
+
+    int k=n,rev=0;
+     while(n!=0)
+     {
+    int r=n%10;
+    if(rev>Integer.MAX_VALUE/10 || rev<Integer.MIN_VALUE/10)
+    return false;
+     rev=(rev*10)+r;
+     n/=10;
+     }
+     if(rev==k) 
+     return true;
+     else
+     return false;  
+    }
+}
