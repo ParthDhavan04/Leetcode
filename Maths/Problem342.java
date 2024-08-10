354590f35e5be8b07f5348f9ca9a79ac12d4c429
Parth Dhavan <parthdhavan2004@gmail.com>
2024-08-10 14:37:17 +0530

    Power of Four

---
+++ Maths/Problem342.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+
+        if(n==1){
+            return true;
+        }
+        if(n<=0){
+            return false;
+        }  
+        while(n!=0  && n!=4){
+            if(n%4!=0){
+                return false;
+            }
+            n=n/4;
+        }
+        return true;
+    }
+}
