4cbc983ab742c54cf6028deb1944e5226395ff38
Parth Dhavan <parthdhavan2004@gmail.com>
2024-08-22 23:05:28 +0530

    Reverse Strings

---
+++ Strings/Problem 344.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int start=0;
+        int end=s.length-1;
+
+        while(start<end){
+            swap(s,start,end);
+            start++;
+            end--;
+        }
+    }
+
+    void swap(char arr[],int a,int b){
+        char temp=arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
+    }
+}
