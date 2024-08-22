4af2461aa9253e0b852f76143bb43424b8da2728
Parth Dhavan <parthdhavan2004@gmail.com>
2024-08-22 22:56:31 +0530

    Reverse Words in a String

---
+++ Strings/Problem 151.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String reverseWords(String s) {
+        s = s.trim();
+        String[] str = s.split("\\s+"); // Use regex to handle multiple spaces
+        StringBuilder sb = new StringBuilder();
+    
+        for (int i = str.length - 1; i >= 0; i--) {
+            sb.append(str[i]);
+            if (i != 0) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+}
