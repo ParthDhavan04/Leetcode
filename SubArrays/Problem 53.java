5372b05a77916e440b13f01ca9a080b653141b8d
Parth Dhavan <parthdhavan2004@gmail.com>
2024-09-28 11:33:58 +0530

    Maximum subarray

---
+++ SubArrays/Problem 53.java
@@ -0,0 +1,14 @@
+class Solution {
+    //KADANE'S ALGORITHM
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        int best=Integer.MIN_VALUE;
+
+        for(int i=0;i<n;i++){
+            sum=Math.max(nums[i],sum+nums[i]);
+            best=Math.max(best,sum);
+        }
+        return best;
+    }
+}
